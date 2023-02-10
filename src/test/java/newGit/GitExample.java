ff61cf8f04fe19e7c0c657e121b27ae20ece186d
kotteswaranpvr <kottipvr@gmail.com>
2023-02-10 09:55:37 +0530

    message

---
+++ src/test/java/newGit/GitExample.java
@@ -0,0 +1,13 @@
+package newGit;
+
+public class GitExample {
+private void main1() {
+	// TODO Auto-generated method stub
+System.out.println("main1");
+
+}
+private void main2() {
+	// TODO Auto-generated method stub
+System.out.println("main2");
+}
+}
