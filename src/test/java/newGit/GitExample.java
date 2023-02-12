1e25cebff9f5dc8ef1f290fbf528440582fd33f8
eswar2017 <kotte1982@hotmail.com>
2023-02-12 22:28:24 +0530

    hotmailadded

--- src/test/java/newGit/GitExample.java
+++ src/test/java/newGit/GitExample.java
@@ -9,5 +9,26 @@ System.out.println("main1");
 private void main2() {
 	// TODO Auto-generated method stub
 System.out.println("main2");
+
+
+}
+
+private void hotmail1() {
+	// TODO Auto-generated method stub
+System.out.println("main2");
+
+
+}
+private void hotmail2() {
+	// TODO Auto-generated method stub
+System.out.println("main2");
+
+
+}
+private void hotmail3() {
+	// TODO Auto-generated method stub
+System.out.println("main2");
+
+
 }
 }
